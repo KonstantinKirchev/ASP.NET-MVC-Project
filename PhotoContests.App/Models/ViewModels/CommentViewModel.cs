aee8a2706626287a91629c9390698291724dc6e5
goodlifeinc <genata93@gmail.com>
2015-10-25 14:05:10 +0200

    Added some comments templates, removed bin/ and obj/ folders from project

--- PhotoContests.App/Models/ViewModels/CommentViewModel.cs
+++ PhotoContests.App/Models/ViewModels/CommentViewModel.cs
@@ -9,6 +9,8 @@ namespace PhotoContests.App.Models.ViewModels
 {
     public class CommentViewModel
     {
+        public int Id { get; set; }
+
         public string Author { get; set; }
 
         public string Content { get; set; }
